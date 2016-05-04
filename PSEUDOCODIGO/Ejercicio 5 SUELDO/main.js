bd4b2fcc1f11649aab11ee28451c1f7eb5854710
lisbethbalabarca <lisbethbalabarca@gmail.com>
2016-05-04 11:00:15 -0500

    avances

--- PSEUDOCODIGO/Ejercicio 5 SUELDO/main.js
+++ PSEUDOCODIGO/Ejercicio 5 SUELDO/main.js
@@ -1,1 +1,10 @@
-//Escribe aquí tu código
+function sueldo () {
+	var h = parseFloat(prompt("ingrese las horas que trabajo"));
+	var p = parseInt(prompt("cual es el valor de la hora trabajada"));
+	var total = 0;
+
+	total = (h*p).toFixed(2);
+	alert("el total del pago es : " + total);
+}
+
+sueldo();
--- PSEUDOCODIGO/Ejercicio 6 CONVERSION MEDIDAS DE LA TELA/main.js
+++ PSEUDOCODIGO/Ejercicio 6 CONVERSION MEDIDAS DE LA TELA/main.js
@@ -1,1 +1,10 @@
-//Escribe aquí tu código
+function conversion () {
+	var t = parseFloat(prompt("Cuantos metros de tela son?:"));
+	var p = 0.0254;
+	var total = 0;
+
+	total = (t/p).toFixed(2);
+	alert("la cantidad de tela que necesitas es: " + total);
+}
+
+conversion();
